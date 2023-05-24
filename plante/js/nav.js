057ce1250d368f77c6edbc834e978b78964f9aa6
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2023-05-24 21:00:18 +0200

    Delete plante directory

--- plante/js/nav.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementById('menu-toggle');
-  checkbox.addEventListener('change', function () {
-    const menuItems = document.querySelector('.menu-items');
-    if (this.checked) {
-      menuItems.style.transform = 'translateX(0)';
-    } else {
-      menuItems.style.transform = 'translateX(-150%)';
-    }
-  });
